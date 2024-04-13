1a5910ea39c03d8b234b046228f6b49cfe43aee8
Колодницкий Илья <80146551+KolodnitskyIlya@users.noreply.github.com>
2024-04-14 01:34:26 +0600

    MyFirstProject

---
+++ MenuMyFirstProject.swift
@@ -0,0 +1,34 @@
+//
+//  MenuMyFirstProject.swift
+//  MyFirstProject
+//
+//  Created by macbook on 14.04.2024.
+//
+
+import SwiftUI
+
+struct MenuMyFirstProject: View {
+    var body: some View {
+        Menu("Click me!") {
+            Button("One") {
+                
+            }
+            
+            Button("Two") {
+                
+            }
+            
+            Button("Three") {
+                
+            }
+            
+            Button("Four") {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuMyFirstProject()
+}
